295abaccd73f72e0a5092419d798feb3d361a672
nitesh-ui <112393577+nitesh-ui@users.noreply.github.com>
2022-09-14 03:24:34 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,62 +1,56 @@
+import { useRef } from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 
-const App = () => {
+function App() {
 
-  const [calc, setCalc] = useState("");
   const [result, setResult] = useState("");
+  const inputRef = useRef(null);
 
-  const ops = ['/', '*', '+', '-', '.'];
+  useEffect(() => inputRef.current.focus());
 
-  const updateCalc = value => {
-    if(
-      (ops.includes(value) && calc === '') ||
-      (ops.includes(value) && ops.includes(calc.slice(-1)
-      ))
-    ) {
-        return;
-    }
-
-    setCalc(calc + value);
-
-    if(!ops.includes(value)) {
-      setResult(eval(calc + value).ToNumber);
-    }
+  const handleClick = (e) => {
+    setResult(result + e.target.name);
   }
 
-  const createDigits = () => {
-    const digits = [];
+  const backSpace = () => {
+    setResult(result.slice(0, result.length-1));
+  }
 
-    for(let i = 1; i < 10; i++) {
-      digits.push(
-        <button key={i} onClick={() => updateCalc(i.toString())}>{i}</button>
-      )
-    }
+  const clear = () => {
+    setResult("");
+  }
 
-    return digits;
+  const calculateResult = () => {
+    setResult(eval(result).toString());
   }
 
   return (
-    <div className="App">
-      <div className="calculator">
-        <div className="display">
-          {result ? <span>({result})</span> : ''}{calc || "0"}
-        </div>
+    <div className="calculator-app">
+      <form>
+        <input type="text" value={result} ref={inputRef} />
+      </form>
 
-        <div className="operators">
-          <button onClick={() => updateCalc('/')}>/</button>
-          <button onClick={() => updateCalc('*')}>*</button>
-          <button onClick={() => updateCalc('+')}>+</button>
-          <button onClick={() => updateCalc('-')}>-</button>
-          <button>DEL</button>
-        </div>
-
-        <div className="digits">
-          {createDigits()}
-          <button onClick={() => updateCalc('0')}>0</button>
-          <button onClick={() => updateCalc('.')}>.</button>
-          <button>=</button>
-        </div>
+      <div className="keypad">
+        <button id="clear" onClick={clear}>Clear</button>
+        <button id="backspace" onClick={backSpace}>C</button>
+        <button name="+" onClick={handleClick}>+</button>
+        <button name="7" onClick={handleClick}>7</button>
+        <button name="8" onClick={handleClick}>8</button>
+        <button name="9" onClick={handleClick}>9</button>
+        <button name="-" onClick={handleClick}>-</button>
+        <button name="4" onClick={handleClick}>4</button>
+        <button name="5" onClick={handleClick}>5</button>
+        <button name="6" onClick={handleClick}>6</button>
+        <button name="*" onClick={handleClick}>*</button>
+        <button name="1" onClick={handleClick}>1</button>
+        <button name="2" onClick={handleClick}>2</button>
+        <button name="3" onClick={handleClick}>3</button>
+        <button name="/" onClick={handleClick}>/</button>
+        <button name="0" onClick={handleClick}>0</button>
+        <button name="." onClick={handleClick}>.</button>
+        <button id="result" onClick={calculateResult}>Result</button>
       </div>
     </div>
   );
